cff2fdaa1b021e979f20303a83609e16888ed3cb
James <james@prestwi.ch>
2022-02-17 18:54:19 -0800

    chore: delete some legacy code

--- package_it.sh
+++ package_it.sh
@@ -54,13 +54,3 @@ for F in "node/$PKG_NAME""_bg.wasm" "node/$PKG_NAME""_bg.d.ts" "node/$PKG_NAME.j
 do
     cat ts/package.json | jq --arg f "$F" '.files += [$f]' > TMP_FILE && mv TMP_FILE ts/package.json
 done
-
-# # # #
-
-# # Merge nodejs & browser packages
-# cp "pkg-node/${PKG_NAME}.js" "pkg/${PKG_NAME}_main.js"
-# sed "s/require[\(]'\.\/${PKG_NAME}/require\('\.\/${PKG_NAME}_main/" "pkg-node/${PKG_NAME}_bg.js" > "pkg/${PKG_NAME}_bg.js"
-# jq ".files += [\"${PKG_NAME}_bg.js\"]" pkg/package.json \
-#     | jq ".main = \"${PKG_NAME}_main.js\"" > pkg/temp.json
-# mv pkg/temp.json pkg/package.json
-# rm -rf pkg-node
