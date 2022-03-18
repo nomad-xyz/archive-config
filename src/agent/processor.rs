716ac16ab7b7ce6ac4d97d440c275777d6bc503a
Luke Tchang <ltchang@stanford.edu>
2022-03-18 10:51:25 -0700

    feature: add agent-specific configs

---
+++ src/agent/processor.rs
@@ -0,0 +1,27 @@
+//! Processor public configuration
+
+use crate::decl_config;
+use ethers::types::H256;
+use std::collections::HashSet;
+
+decl_config!(Processor {
+    /// Allow list
+    allowed: Option<HashSet<H256>>,
+    /// Deny list
+    denied: Option<HashSet<H256>>,
+    /// Index only mode
+    #[serde(default)]
+    index_only: bool,
+    /// S3 config
+    s3: Option<S3Config>,
+});
+
+/// S3 Configuration
+#[derive(Debug, Default, serde::Serialize, serde::Deserialize, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct S3Config {
+    /// Bucket
+    pub bucket: String,
+    /// Region
+    pub region: String,
+}
